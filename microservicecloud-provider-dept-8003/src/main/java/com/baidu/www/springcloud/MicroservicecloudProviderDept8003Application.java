a50e8d7df48ec7a2aefc0c9b92e4d0065aa352e1
liuyang <shenshiyishao@163.com>
2018-09-10 15:28:47 +0800

    instance:       instance-id: microservicecloud-dept8003  #自定义服务名称信息

--- microservicecloud-provider-dept-8003/src/main/java/com/baidu/www/springcloud/MicroservicecloudProviderDept8003Application.java
+++ microservicecloud-provider-dept-8003/src/main/java/com/baidu/www/springcloud/MicroservicecloudProviderDept8003Application.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @EnableDiscoveryClient //服务发现
-public class MicroservicecloudProviderDept8002Application {
+public class MicroservicecloudProviderDept8003Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroservicecloudProviderDept8002Application.class, args);
+        SpringApplication.run(MicroservicecloudProviderDept8003Application.class, args);
     }
 }
