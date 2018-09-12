663d5c8bda44be93bf44b13e868590790c9c0a84
liuyang <shenshiyishao@163.com>
2018-09-12 17:58:21 +0800

    增加负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/baidu/www/springcloud/configbean/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/baidu/www/springcloud/configbean/ConfigBean.java
@@ -1,10 +1,18 @@
 package com.baidu.www.springcloud.configbean;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
+/**
+ * 配置文件
+ * @author Administrator
+ */
 @Configuration
 public class ConfigBean {
 
@@ -12,6 +20,17 @@ public class ConfigBean {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 用重新选择的随机算法,替代默认的算法
+     *
+     * @return
+     */
+    @Bean
+    public IRule myRule() {
+
+        return new RandomRule();
     }
 
 }
