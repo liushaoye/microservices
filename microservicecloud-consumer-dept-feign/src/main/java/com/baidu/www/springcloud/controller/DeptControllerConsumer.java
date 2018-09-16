42d836d4ba1440d6bcad99684da3e30c964ae98f
liuyang <shenshiyishao@163.com>
2018-09-16 10:17:18 +0800

    feign结束

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/baidu/www/springcloud/controller/DeptControllerConsumer.java
@@ -0,0 +1,36 @@
+package com.baidu.www.springcloud.controller;
+
+import java.util.List;
+
+import com.baidu.www.springcloud.entity.Dept;
+import com.baidu.www.springcloud.service.DeptClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Administrator
+ */
+
+@RestController
+public class DeptControllerConsumer {
+    @Autowired
+    private DeptClientService service;
+
+    @RequestMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        return this.service.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list() {
+        return this.service.list();
+    }
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public Object add(Dept dept) {
+        return this.service.add(dept);
+    }
+}
